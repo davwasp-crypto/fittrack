2f972360bbe8bb6953084b376efe64a5541e30f1
davide vespa <dav.wasp@gmail.com>
2026-04-28 20:28:05 +0200

    frontend

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post(
+                "http://localhost:5000/api/auth/login",
+                {
+                    email,
+                    password,
+                }
+            );
+
+            console.log("LOGIN SUCCESS:", res.data);
+
+            // salva token
+            localStorage.setItem("token", res.data.token);
+
+            alert("Login riuscito 🚀");
+        } catch (error) {
+            console.error(error);
+            alert("Errore login");
+        }
+    };
+
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h2>Login</h2>
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
